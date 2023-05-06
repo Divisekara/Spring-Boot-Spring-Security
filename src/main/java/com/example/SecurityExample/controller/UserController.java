a052efa8f8f8bc873f2059930a70afb752bf12a7
asitha divisekara <asithad@mybudget.com.au>
2023-05-07 02:12:55 +0530

    added first RestController and RequestMapping and getEndpoing

--- src/main/java/com/example/SecurityExample/controller/UserController.java
+++ src/main/java/com/example/SecurityExample/controller/UserController.java
@@ -1,2 +1,14 @@
-package com.example.SecurityExample.controller;public class UserController {
+package com.example.SecurityExample.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class UserController {
+    @GetMapping
+    public String userGetHandler(){
+        return "asitha divisekara";
+    }
 }
